18d330fb90b21b628eff0b64d68011b96a19389d
Fidelis Wambui <fidelis.wambui@emalify.com>
2024-06-04 16:15:39 +0300

    Feature:Added middleware file to verify authentication

--- routes/api.php
+++ routes/api.php
@@ -8,3 +8,9 @@ Route::group([],function(){
     Route::post('/signup', [UserProfileController::class, 'signUp']) ->name('signup');
     Route::post('/login', [UserProfileController::class, 'login'])->name('login');
     });
+
+Route::middleware('jwt.verify')->group(function(){
+    Route::get('/dashboard', function(){
+        return response()->json(['message' =>'Welcome to the dashboard'], 200);
+    });
+});
