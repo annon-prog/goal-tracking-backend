1a6b3db0362f62bb0f1a16209afb429441b56128
Fidelis Wambui <fidelis.wambui@emalify.com>
2024-06-07 15:00:15 +0300

    FIX:debugged an issue connecting routes to a middleware by using the new laravel 11 standards

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,14 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserProfileController;
-
-
+use App\Http\Middleware\JWTMiddleware;
 
 Route::group([],function(){
     Route::post('/signup', [UserProfileController::class, 'signUp']) ->name('signup');
     Route::post('/login', [UserProfileController::class, 'login'])->name('login');
     });
 
-Route::middleware('jwt.verify')->group(function(){
+Route::middleware([JWTMiddleware::class])->group(function(){
     Route::get('/dashboard', function(){
         return response()->json(['message' =>'Welcome to the dashboard'], 200);
     });
